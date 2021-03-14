12b14f34fe63d3c18049c740e474e2e8198fac5c
93TEI <93louis@gmail.com>
2021-03-14 16:44:52 +0900

    (주석으로 표시)get,set을 쓰는 방법이 구조체 안에 함수를 쓰는 것 같은데 대입하는 형식으로 사용함

--- 객체 심화.js
+++ 객체 심화.js
@@ -53,3 +53,38 @@ let num ={
 console.log(num.sum);
 
 //set
+
+let number = {
+  _a : 1,
+  _b : 2,
+  sum : 3,
+  calculate(){
+    console.log('calculate');
+    this.sum = this._a + this._b;
+  },
+  get a(){
+    return this._a;
+  },
+  get b(){
+    return this._b;
+  },
+  set a(value){
+    console.log('a가 바뀝니다.');
+    this._a = value;
+    this.calculate()
+  },
+  set b(value){
+    console.log('b가 바뀝니다.');
+    this._b = value;
+    this.calculate()
+  }
+}
+
+console.log(number.sum);
+//구조체같이 생긴거에 함수를 쓰는것 같은데 이 방식이 다름
+number.a = 5;
+number.b = 7;
+number.a = 9;
+console.log(number.sum);
+console.log(number.sum);
+console.log(number.sum);
