f685227e10efc280246aed9d7bedb2bc05feab5b
93TEI <93louis@gmail.com>
2021-03-14 16:50:43 +0900

    객체배열, 내장함수 push의 사용

---
+++ 배열과 객체배열.js
@@ -0,0 +1,11 @@
+//배열
+let array = [1,2,3,4,5];
+
+//객체배열
+let objects = [{name:'멍멍이'},{name:'야옹이'}];
+console.log(objects[1]);
+
+//내장함수 push
+objects.push({name:'괄괄이'});
+console.log(objects.length);
+
