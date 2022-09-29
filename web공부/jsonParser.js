3500ed84a41ea1213d6033f698f7677efc274d13
93TEI <43162525+93TEI@users.noreply.github.com>
2022-09-29 17:36:24 +0900

    Update jsonParser.js

--- web공부/jsonParser.js
+++ web공부/jsonParser.js
@@ -1,6 +1,24 @@
 // 220929 이프로님
 // JSON 다국어 예제
+//// + 최팀장님 아이디어 추가해봄
 
+patient = {
+    title: { en: 'Patient', ko: '환자' },
+    item: {
+        registration: { en: 'Registration', ko: '등록'},
+        delete:{ en: 'Delete', ko: '삭제'},
+        tab : {
+            title: {en : "Category", ko:"카테고리"},
+            item: {
+                tab1:{
+                    title: {en:"first tab", ko: "첫번째 메뉴"},
+                    
+                },
+                tab2:{en:"second tab", ko: "두번째 메뉴"}
+            }
+        }
+    }
+}
 data = {
     first: "first string",
     second: "second string",
@@ -11,11 +29,13 @@ data = {
     }
 }
 
-query = "child1.first";
+//query = "child1.first";
+//result = data;
 
+query = "item.tab.title.ko";
 queryList = query.split(".");
 
-result = data;
+result = patient;
 
 for (i of queryList) {
     result = result[i];
